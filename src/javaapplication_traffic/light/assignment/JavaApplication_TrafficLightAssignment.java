f7a18711cbc0193a3c74a92849a0a979a115b94e
James Dibo <jeffedibo@gmail.com>
2018-06-21 16:03:47 +0300

    Add all files

---
+++ src/javaapplication_traffic/light/assignment/JavaApplication_TrafficLightAssignment.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.0
+ */
+package javaapplication_traffic.light.assignment;
+
+/**
+ *
+ * @author DIBO
+ */
+public class JavaApplication_TrafficLightAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
